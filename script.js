01648db9a4e94da6b4e27e80668d3ead3b303b0d
sonamrani1 <129504719+sonamrani1@users.noreply.github.com>
2024-06-27 11:52:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+
+
+/*=============toggle icon navbar===============*/
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+
+// /*======================= scroll section active link====================*/
+
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
+
+// window.onscroll = () => {
+//     sections.foreEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if (top >= offset && top < offset + height) {
+//             navLinks.forEach.apply(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a[href*=' + id + ']').classlist.add('active');
+//             });
+//         };
+//     });
+
+//     /*=========================sticky navbar=========================*/
+//     let header = document.querySelector('header');
+//     header.classList.toggle('sticky', window.scrollY > 100);
+    
+
+
+//     /*=======================remove toggle icon and navbar===================*/
+//     menuIcon.classList.remove('fa-xmark');
+//     navbar.classList.remove('active');
+
+// };
+
+
+
+/*=======================scroll reveal===================*/
+
+// ScrollReveal({
+//     distance: '80px',
+//     duration:2000,
+//     delay:100,
+// });
+
+// ScrollReveal().reveal('.home-content,heading',{origin: 'top'});
+// ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form',{origin: 'bottom'});
+// ScrollReveal().reveal('.home-content h1,.about-img',{origin: 'left'});
+// ScrollReveal().reveal('.home-content p,about-content',{origin: 'right'});
+
+
+
+
+
+
+
+
